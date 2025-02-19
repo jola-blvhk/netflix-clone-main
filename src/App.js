8b6ec92e7df265588bd05e63ab93c0f38b2e3339
jola-blvhk <awujoolabello@gmail.com>
2025-02-19 10:14:36 +0100

    changed for deployment

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useEffect} from "react";
+import "./App.css";
+import { HomeScreen } from "./Components/Screens/HomeScreen";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { LoginScreen } from "./Components/Screens/LoginScreen";
+import { ProfileScreen } from "./Components/Screens/ProfileScreen";
+import { auth } from "./firebase";
+import { login, logout, selectUser } from "./features/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+
+
+function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() =>{
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if(userAuth) {
+        //Logged in
+        console.log(userAuth);
+        dispatch(login({
+          uid: userAuth.uid, 
+          email: userAuth.email,
+        })
+        );
+      } else {
+        //Logged out
+        dispatch(logout());
+      }
+    });
+
+    return unsubscribe;
+
+  }, [dispatch]);
+
+
+  return (
+    <div className="BGC">
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
+            <Route exact path="/" element={<HomeScreen />} />
+            <Route exact path="/profile" element={<ProfileScreen />} />
+
+          </Routes>
+        )}
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Screens/HomeScreen.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import { Nav } from "../Nav";
+import requests from "../../Request";
+import { Row } from "../Row";
+import CloudflareBanner from "../CloudflareBanner";
+import { fetchVideos } from "../../app/cloudflare";
+
+export const HomeScreen = () => {
+  const [videos, setVideos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const loadVideos = async () => {
+      try {
+        setLoading(true);
+        const fetchedVideos = await fetchVideos();
+        setVideos(fetchedVideos);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadVideos();
+  }, []);
+
+  console.log(videos);
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen text-white">
+        Loading...
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="flex items-center justify-center h-screen text-white">
+        Error: {error}
+      </div>
+    );
+  }
+
+  return (
+    <div className="">
+      <Nav />
+      <CloudflareBanner videos={videos} />
+
+      <Row
+        title="NETFLIX ORGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
+};
